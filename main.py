e34e6a48fb3eb54afe6653c87c7885c300641d5f
Ojas Mittal <93815296+OjasMittal@users.noreply.github.com>
2022-03-10 13:03:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import pandas
+num=""
+df=pandas.read_excel('people.xlsx')
+for index,row in df.iterrows():
+        a= str(row['number'])
+        num+=a+','
+l=len(num)
+num=num[:l-1]
+url= "https://www.fast2sms.com/dev/bulkV2"
+message="Hello, How are you?"
+payload=f'sender_id=Cghpet&message={message}&route=v3&language=english&numbers={num}'
+headers={
+  'authorization':"xXOqoyearA7mESlHnjsz4LK15tBG0vF9pcQNWUifPwVhg2J6dCAMFWouO8ZKE9pc1tmBiqr7VNykQXTh",
+'Content-Type':'application/x-www-form-urlencoded'
+  
+}
+response=requests.request("POST",url=url,data=payload,headers=headers)
+print(response.text)
